d7684d073c24a3e88e0fdac24a0e131cbeaf50ed
Scarlet Barbosa <scarlet.profissional@gmail.com>
2023-05-02 19:42:18 -0300

    Update jogo

---
+++ 02-PreProjetos/MarioMemory/src/js/database.js
@@ -0,0 +1,22 @@
+const personagens = [
+    {
+      id: "0",
+      name: "boo",
+      img: "./MarioMemory/src/img/Boo.png",
+    },
+    {
+      id: "1",
+      name: "goomba",
+      img: "./MarioMemory/src/img/Goomba.png",
+    },
+    {
+      id: "2",
+      name: "shell",
+      img: "./MarioMemory/src/img/Shell.png",
+    },
+    {
+      id: "3",
+      name: "piranha",
+      img: "./MarioMemory/src/img/Piranha.png",
+    },
+  ];
